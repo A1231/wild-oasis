4e27771c1197b9002434e98f5a0d3515e86de891
A1231 <37673364+A1231@users.noreply.github.com>
2025-09-14 17:43:39 -0400

    add global styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,13 @@
 import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
 const H1 = styled.h1`
   font-size: 30 px;
   font-weight: 600;
   background-color: red;
 `; //creates a randomly named class and applies to the element
 //scoped only to this componenet
-
-const Button = styled.button`
-  background-color: purple;
-  padding: 1.2 rem 1.6 rem;
-  color: white;
-  font-size: 1.4rem;
-  cursor: pointer;
-  margin: 20px;
-`;
-const Input = styled.input`
-  border: 1px solid gray;
-  border-radius: 4px;
-  padding: 0.8rem 1.2rem;
-  font-size: 1.4rem;
-`;
 
 //styling the component itself conventionally named StyledComponentName
 const StyledApp = styled.div`
@@ -28,12 +16,15 @@ const StyledApp = styled.div`
 `;
 function App() {
   return (
-    <StyledApp>
-      <H1>Wild Oasis</H1>
-      <Button onClick={() => alert("Check in ")}>Check In</Button>
-      {/* don't need t create a React component and manually pass to an HTMl element*/}
-      <Input placeholder="Number of guests" type="number" />
-    </StyledApp>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>Wild Oasis</H1>
+        <Button onClick={() => alert("Check in ")}>Check In</Button>
+        {/* don't need t create a React component and manually pass to an HTMl element*/}
+        <Input placeholder="Number of guests" type="number" />
+      </StyledApp>
+    </>
   );
 }
 
